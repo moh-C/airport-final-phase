337835b0e12007ce0788348de84b6c143d2af6bb
Aaron <mohammad.chegini@hotmail.com>
2019-09-25 10:19:45 +0330

    Initial setup after restard

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
+// Bringing in the moduels
 const express = require('express');
+const expbs = require('express-handlebars');
+
+// Bringing in the app
 const app = express();
-const path = require('path');
-const exphbs = require('express-handlebars');
-
-// Setting the templating engine
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-
-// Setting the parsers
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// Implementing the routers
-app.use('/', require('./Routes/staticPages'));
 
 // Setting the port
-const PORT = process.env.PORT || 5000;
-
-// Setting the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+const PORT = process.env.PORT;
+// Starting the server
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));

da12ac3aa0c259de0ba6a15fa778b224560e8534
Aaron <mohammad.chegini@hotmail.com>
2019-09-25 10:29:05 +0330

    First stable shit

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const expbs = require('express-handlebars');
 // Bringing in the app
 const app = express();
 
+// Setting the view engine
+app.engine('handlebars', expbs());
+app.set('view engine', 'handlebars');
+
+// Routing
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
 // Setting the port
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 // Starting the server
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));

1edcd5c42c06e5eb4ac96a184b72721298af0a1c
Aaron <mohammad.chegini@hotmail.com>
2019-09-26 10:39:44 +0330

    homepage is done yaya

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
 // Bringing in the moduels
 const express = require('express');
-const exphbs = require('express-handlebars');
+const path = require('path');
 
 // Bringing in the app
 const app = express();
-
-// Setting the view engine
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-
-// Routing
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'سامانه پایش فرودگاه های کشور'
-  });
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
 
 // Setting the port
 const PORT = process.env.PORT || 5000;

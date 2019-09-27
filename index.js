532b6b7b2f8988ba3eda7a43fc7f60761d6394b8
Aaron <mohammad.chegini@hotmail.com>
2019-09-27 11:14:44 +0330

    shit's better now

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const path = require('path');
 // Bringing in the app
 const app = express();
 
+app.use('/', express.static(path.resolve(__dirname, 'assets')));
+
+app.get('/home.html', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'Pages', 'Home', 'index.html'));
+});
+
 // Setting the port
 const PORT = process.env.PORT || 5000;
 // Starting the server

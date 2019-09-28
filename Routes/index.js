d02388929a586da50acee28ef320280c54776f99
Aaron <mohammad.chegini@hotmail.com>
2019-09-28 19:50:27 +0330

    Maintenance Guidelines main

--- Routes/index.js
+++ Routes/index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/home', (req, res) => {
+router.get('/home.html', (req, res) => {
   res.render('home');
 });
 
-router.get('/airport', (req, res) => {
+router.get('/airport.html', (req, res) => {
   res.render('airport');
 });
 
-router.get('/login',(req,res)=>{
-  res.render('login')
-})
+router.get('/login.html', (req, res) => {
+  res.render('login');
+});
+
+router.get('/Maintenance/Main.html', (req, res) => {
+  res.render('maintenanceGuidelineMain');
+});
 
 module.exports = router;

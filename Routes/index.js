e477ecd6e3a7e5e21e7a6b08955fade80db5283b
Aaron <mohammad.chegini@hotmail.com>
2019-09-30 19:45:40 +0330

    Home page doen

--- Routes/index.js
+++ Routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.redirect('/Home.html');
+});
+
 router.get('/Home.html', (req, res) => {
   res.render('home');
 });
